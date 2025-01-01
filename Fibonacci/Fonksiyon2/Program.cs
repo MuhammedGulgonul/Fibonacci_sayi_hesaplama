ebdbe90404ce91350d17f4ad61c7150f9ac4f590
Muhammed Fatih Gülgönül <160837491+MuhammedGulgonul@users.noreply.github.com>
2025-01-01 12:52:37 +0300

    Add files via upload

---
+++ Fibonacci/Fonksiyon2/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Fonksiyon2
+{
+    public static void Main(string[] args)
+    {
+
+   dongu(1,1,11);//1 e 1 den 11. fobinacci sayısına kadar gider
+
+    }
+    public static int dongu(int i,int x,int n)
+    {
+     
+
+        if (n<=0)
+        {
+            return 0;
+        }
+        else
+        {
+            if (n != 1)
+            {
+                Console.Write(i + " " + x + " ");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+
+            i = i + x;
+            x = i + x;
+
+          
+            return dongu(i,x,n-2);
+        }
+       
+    }
+}
+
+
